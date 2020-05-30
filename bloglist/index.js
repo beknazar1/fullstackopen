b3a3427c42b11b838649de18bc71e552207a5365
beknazar1 <5906333+beknazar1@users.noreply.github.com>
2020-05-30 15:56:19 -0500

    4.2 config separated

--- bloglist/index.js
+++ bloglist/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 const blogSchema = mongoose.Schema({
@@ -15,7 +15,6 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
@@ -39,7 +38,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
